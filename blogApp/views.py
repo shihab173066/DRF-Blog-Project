1422f7ad5ccd86b76c2c24ca42bd3c08ccf7fc23
shihab173066 <shihab.hossainSH@gmail.com>
2025-10-08 19:54:30 +0600

    views setup

--- blogApp/views.py
+++ blogApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def index(request):
+
+    return Response({"Success": "The setup was successful"})
