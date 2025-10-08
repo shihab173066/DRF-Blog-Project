da934167c38fd7d947b897eaeb0438b79d1cbc91
shihab173066 <shihab.hossainSH@gmail.com>
2025-10-09 01:25:33 +0600

    tested API

--- blogApp/tests.py
+++ blogApp/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+# import requests
 
-# Create your tests here.
+class SimpleAPITest(TestCase):
+    def test_api_home(self):
+        response = self.client.get('http://127.0.0.1:8000')  # Django's internal client
+        print(response.status_code)
+        print(response.content)
+        self.assertEqual(response.status_code, 200)
--- blogApp/views.py
+++ blogApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Post
-from .serializers import PostSerializer
+# from .serializers import PostSerializer
 
 @api_view(['GET'])
 def index(request):
