2532dc122b08654e7af345c3c76d7b168be8f92a
shihab173066 <shihab.hossainSH@gmail.com>
2025-10-09 04:04:36 +0600

    created serializers and end points

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    # path('get-all-posts/', views.GetAllPosts),
-    # path('create-new-post/', views.CreatePost) ,
-    # path('delete-post/', views.DeletePost),
-    # path('get-post/', views.GetPost),
-    # path('update-post/', views.UpdatePost),
+    path('get-all-posts/', views.GetAllPosts),
+    path('create-new-post/', views.CreatePost) ,
+    path('delete-post/', views.DeletePost),
+    path('get-post/', views.GetPost),
+    path('update-post/', views.UpdatePost),
 ]
